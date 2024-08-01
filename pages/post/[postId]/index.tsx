a827f01c16934ae52d4641b8eef652cee461bf7e
Faysal Alam <faysal.alam@sazim.io>
2024-08-01 12:09:47 +0600

    added data fetchin

--- pages/post/[postId]/index.tsx
+++ pages/post/[postId]/index.tsx
@@ -1,9 +1,53 @@
-import { useRouter } from "next/router";
 import React from "react";
+import { useRouter } from "next/router";
 
-const Post = () => {
-  const router = useRouter();
-  return <div>Post of id {router.query.postId}</div>;
+interface IPost {
+  id: number;
+  title: string;
+}
+
+type TProps = {
+  post: IPost;
 };
+
+interface IParams {
+  postId: string;
+}
+
+type TStaticProps = {
+  params: IParams;
+};
+
+const Post = ({ post }: TProps) => {
+  const router = useRouter();
+  return (
+    <div>
+      Post of id {router.query.postId}
+      <div>
+        <p>Post Title: {post?.title}</p>
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = (await res.json()) as IPost[];
+
+  const paths = posts.map((post) => {
+    params: {
+      postId: post.id.toString();
+    }
+  });
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }: TStaticProps) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const post = res.json();
+  return { props: { post } };
+}
 
 export default Post;
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-const Posts = () => {
-  return <div>Posts</div>;
+interface IPost {
+  id: number;
+  title: string;
+}
+
+type TProps = {
+  data: IPost[];
 };
+
+const Posts = ({ data }: TProps) => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {data.map((post) => (
+          <p key={post.id}>{post.title}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { props: { data } };
+}
 
 export default Posts;
